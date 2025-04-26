e9bca6c63f36d3ac86b8ef71804bac33dc5d898a
INHOUSE-8 <inhouse8@ednindo.net>
2025-04-26 17:57:02 +0700

    add sms send

--- src/views/SMSSenderView.vue
+++ src/views/SMSSenderView.vue
@@ -4,40 +4,46 @@
             <h2 class="mb-4 fw-bold">SMS Sender</h2>
             <div class="card">
                 <div class="card-body">
-                <form class="mb-4">
-                    <div class="mb-3">
-                        <input v-model="phoneNumber" type="text" placeholder="Nomor HP" class="form-control">
+                    <form class="mb-4">
+                        <div class="mb-3">
+                            <input v-model="phoneNumber" type="text" placeholder="Nomor HP" class="form-control">
+                        </div>
+                        <textarea v-model="message" name="" id="" class="form-control"
+                            placeholder="Tulis pesan di sini.." rows="12"></textarea>
+                    </form>
+                    <div class="d-flex">
+                        <button @click="sendSms" class="btn btn-primary m-auto">Kirim</button>
                     </div>
-                    <textarea v-model="message" name="" id="" class="form-control" placeholder="Tulis pesan di sini.." rows="12"></textarea>      
-                </form>
-                <div class="d-flex">
-                    <button @click="sendSms" class="btn btn-primary m-auto">Kirim</button>
                 </div>
             </div>
-        </div>
         </div>
     </div>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            phoneNumber: '',
-            message: ''
-        }
-    },
-    methods: {
-        sendSms() {
-            console.log(this.phoneNumber)
-            console.log(this.message)
-            fetch('http://localhost:8000/api/sms.php?r=send', {
-                body: JSON.stringify({
-                    phoneNumber: this.phoneNumber,
-                    message: this.message
-                })
-            });
+    export default {
+        data() {
+            return {
+                phoneNumber: '',
+                message: ''
+            }
+        },
+        methods: {
+            async sendSms() {
+                try {
+                    const response = await fetch('http://localhost:8000/api/sms.php?r=send', {
+                        method: 'post',
+                        body: {
+                            phoneNumber: this.phoneNumber,
+                            message: this.message
+                        }
+                    });
+                    const result = await response.json();
+                    console.log(result)
+                } catch (error) {
+                    console.error(error);
+                }
+            }
         }
     }
-}
 </script>
